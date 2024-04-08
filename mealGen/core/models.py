0d2a35621f6a462768de9e1b6045974d6e1f5b7f
afeins19 <56986596+afeins19@users.noreply.github.com>
2024-04-08 00:35:47 +0000

    created core models for making users and storing their MyFitnessPal Credentials

--- mealGen/core/models.py
+++ mealGen/core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from cryptography.fernet import Fernet # handling password encryption
+from dotenv import load_dotenv, set_key
+from mealGen.env_generator import generate_key_and_update_env
+
+import os 
+
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    """Holds the user's account information"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField(null=True, blank=True)
+    weight = models.IntegerField(null=True, blank=True)
+    dietary_preferences = models.CharField(max_length=100)
+
+class UserAPICredentials(models.Model):
+    """Holds the users MyFitnessPal Email and Token"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    _encrypted_password = models.CharField(max_length=255)
+
+    # getter and setter for encrypted password data 
+    @property
+    def password(self):
+        # Retrieve the encryption key from the environment variable
+        encryption_key = os.getenv("ENCRYPTION_KEY")
+        f = Fernet(encryption_key.encode())
+
+        # Decrypt the password
+        return f.decrypt(self._encrypted_password.encode()).decode()
+
+    @password.setter
+    def password(self, raw_password):
+        # Retrieve the encryption key from the environment variable
+        encryption_key = os.getenv("ENCRYPTION_KEY")
+        f = Fernet(encryption_key.encode())
+
+        # Encrypt and store the password
+        self._encrypted_password = f.encrypt(raw_password.encode()).decode()
---
+++ mealGen/env_generator.py
@@ -0,0 +1,18 @@
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv, set_key
+import os
+
+def generate_key_and_update_env(env_path='.env', key_name='ENCRYPTION_KEY'):
+    # Load existing .env file or create it if it doesn't exist
+    if not os.path.exists(env_path):
+        with open(env_path, 'w'): pass
+    load_dotenv(env_path)
+
+    # Generate a new encryption key
+    key = Fernet.generate_key()
+
+    # Add or update the encryption key in the .env file
+    set_key(env_path, key_name, key.decode())
+
+if __name__ == "__main__":
+    generate_key_and_update_env()
